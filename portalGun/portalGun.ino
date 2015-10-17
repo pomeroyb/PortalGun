3ccd20b67a4fe89e01366a4c14434f2df6cf2620
Brandon Pomeroy <pomeroyb@uw.edu>
2015-10-16 22:32:23 -0700

    Add ability to put Trinket to sleep

--- portalGun/portalGun.ino
+++ portalGun/portalGun.ino
@@ -5,6 +5,8 @@
 #include "Adafruit_GFX.h"
 #include <ClickEncoder.h>
 #include <TimerOne.h>
+#include <avr/sleep.h>
+#include <avr/power.h>
 
 // Set up our LED display
 Adafruit_AlphaNum4 alpha4 = Adafruit_AlphaNum4();
@@ -18,6 +20,12 @@ int16_t last, value;
 // Comment this line to make the encoder increment in the opposite direction
 #define reverseEncoderWheel
 
+// Set up what we need to sleep/wake the Trinket
+// Define the pins you'll use for interrupts - CHANGE THESE to match the input pins
+// you are using in your project
+#define NAV0_PIN A2
+//#define NAV1_PIN 5
+//#define NAV2_PIN 3
 
 
 void timerIsr() {
@@ -25,16 +33,47 @@ void timerIsr() {
 }
 
 void setup() {
+  enablePinInterupt(NAV0_PIN);
+//  enablePinInterupt(NAV1_PIN);
+//  enablePinInterupt(NAV2_PIN);
+  
   encoderSetup();
   alpha4.begin(0x70);  // pass in the address for the LED display
   
   //uncomment this to make the display run through a test at startup
   //displayTest();
-
 }
 
+void loop() {
+  ClickEncoder::Button b = encoder->getButton();
+  switch (b) {
+    case ClickEncoder::Held:
+      alpha4.clear();
+      alpha4.writeDigitAscii(0, 'H');
+      alpha4.writeDigitAscii(1, 'E');
+      alpha4.writeDigitAscii(2, 'L');
+      alpha4.writeDigitAscii(3, 'D');
+      alpha4.writeDisplay();
+      delay(5000);
+      alpha4.clear();
+      alpha4.writeDisplay();
+      delay(5000);
+      goToSleep();
+    break;
+    case ClickEncoder::DoubleClicked:
+      dimensionLetter = 'C';
+      value = 137;
+    break;
+    case ClickEncoder::Open:
+      updateDimension();
+    break;
+  }
+}
+
+
+
+
 void encoderSetup(){
-  
   // set up encoder
   // unfortunately these Pins are hard coded.
     encoder = new ClickEncoder(A1, A0, A2, 4);
@@ -45,7 +84,6 @@ void encoderSetup(){
     Timer1.attachInterrupt(timerIsr); 
     last = -1;
     value = 137;
-  
 }
 
 
@@ -86,30 +124,75 @@ void updateDimension(){
   alpha4.writeDisplay();
 }
 
-void loop() {
-  ClickEncoder::Button b = encoder->getButton();
-  switch (b) {
-    case ClickEncoder::Held:
-      alpha4.clear();
-      alpha4.writeDigitAscii(0, 'H');
-      alpha4.writeDigitAscii(1, 'E');
-      alpha4.writeDigitAscii(2, 'L');
-      alpha4.writeDigitAscii(3, 'D');
-      alpha4.writeDisplay();
-    break;
-    case ClickEncoder::DoubleClicked:
-      dimensionLetter = 'C';
-      value = 137;
-    break;
-    case ClickEncoder::Open:
-      updateDimension();
-    break;
-  }
+
+
+
+
+/*
+============== Sleep/Wake Methods ==================
+====================================================
+*/
+
+// Most of this code comes from seanahrens on the adafruit forums
+// http://forums.adafruit.com/viewtopic.php?f=25&t=59392#p329418
+
+
+void enablePinInterupt(byte pin)
+{
+    *digitalPinToPCMSK(pin) |= bit (digitalPinToPCMSKbit(pin));  // enable pin
+    PCIFR  |= bit (digitalPinToPCICRbit(pin)); // clear any outstanding interrupt
+    PCICR  |= bit (digitalPinToPCICRbit(pin)); // enable interrupt for the group
+}
+
+void goToSleep()   
+{
+// The ATmega328 has five different sleep states.
+// See the ATmega 328 datasheet for more information.
+// SLEEP_MODE_IDLE -the least power savings 
+// SLEEP_MODE_ADC
+// SLEEP_MODE_PWR_SAVE
+// SLEEP_MODE_STANDBY
+// SLEEP_MODE_PWR_DOWN -the most power savings
+// I am using the deepest sleep mode from which a
+// watchdog timer interrupt can wake the ATMega328
+
+set_sleep_mode(SLEEP_MODE_PWR_DOWN); // Set sleep mode.
+sleep_enable(); // Enable sleep mode.
+sleep_mode(); // Enter sleep mode.
+// After waking the code continues
+// to execute from this point.
+
+sleep_disable(); // Disable sleep mode after waking.                   
+}
+
+ISR (PCINT0_vect) // handle pin change interrupt for D8 to D13 here
+{    
+  // if I wired up D8-D13 then I'd need some code here
+} 
+
+ISR (PCINT1_vect) // handle pin change interrupt for A0 to A5 here // NAV0
+{
+    /* This will bring us back from sleep. */
+  
+  /* We detach the interrupt to stop it from 
+   * continuously firing while the interrupt pin
+   * is low.
+   */
+  detachInterrupt(0);
+}
+
+ISR (PCINT2_vect) // handle pin change interrupt for D0 to D7 here // NAV1, NAV2
+{
+  // Check it was NAV1 or NAV2 and nothing else
 }
 
 
 
 
+/*
+============== Testing Methods ==================
+=================================================
+*/
 
 void displayTest() {
   
